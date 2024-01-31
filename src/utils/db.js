5e3e0b9bf104386fef253a0acf4207716d93465b
CyberLegium192 <cyberlegium@gmail.com>
2024-01-31 19:43:36 +0700

    commit to heroku

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,17 +1,17 @@
 const mysql = require("mysql")
 
-// const con = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: `${process.env.DATABASE_NAME}`,
-// })
 const con = mysql.createConnection({
-  host: `${process.env.DATABASE_HOST}`,
-  port: `${process.env.DATABASE_PORT}`,
-  user: `${process.env.DATABASE_USER}`,
-  password: `${process.env.DATABASE_PASS}`,
-  database: `${process.env.DATABASE_NAME}`
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: `${process.env.DATABASE_NAME}`,
 })
+// const con = mysql.createConnection({
+//   host: `${process.env.DATABASE_HOST}`,
+//   port: `${process.env.DATABASE_PORT}`,
+//   user: `${process.env.DATABASE_USER}`,
+//   password: `${process.env.DATABASE_PASS}`,
+//   database: `${process.env.DATABASE_NAME}`
+// })
 
 module.exports = con
